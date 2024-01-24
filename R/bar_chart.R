0bce702e9d0e4cd40d3c63391b5fe2b957828110
Feller Alessandro (SSZ) <Alessandro.Feller@zuerich.ch>
2024-01-24 09:41:20 +0100

    Bar Chart with correct font

--- R/bar_chart.R
+++ R/bar_chart.R
@@ -7,7 +7,7 @@ library(zueritheme)
 library(data.table)
 library(dplyr)
 library(here)
-library(extrafont)
+library(showtext)
 
 # Data
 URL <- "https://data.stadt-zuerich.ch/dataset/bev_bestand_jahr_od3243/download/BEV324OD3243.csv"
@@ -16,17 +16,22 @@ df <- fread(URL, encoding = "UTF-8")
 # Define Colors
 color <- get_zuericolors(palette = "qual6", nth = 1)
 
-# Import HelveticaNeueLTPro
-font_import(pattern = "HelveticaNeueLTPro-Roman.ttf")
-loadfonts(device = "win")
-windowsFonts()
+# Import HelveticaNeue LT Pro (Change path to where the font is)
+font_add(family = "Helv", 
+         regular = "C:/Path_to_the_Font/HelveticaNeueLTPro-Roman.ttf",
+         bold = "C:/Path_to_the_Font/HelveticaNeueLTPro-Hv.ttf")
+
+# Plotting Resolution Parameters
+showtext_auto()
+showtext_opts(dpi = 300)
 
 # Plot
 plot <- ggplot(data = df,
        aes(x = StichtagDatJahr,
            y = AnzBestWir)) +
   geom_bar(stat = "identity",
-           fill = color) +
+           fill = color,
+           width = 0.7) +
   scale_y_continuous(labels = function(x) format(x, 
                                                  big.mark = " ", 
                                                  scientific = FALSE),
@@ -37,8 +42,8 @@ plot <- ggplot(data = df,
        y = "Anzahl Personen",
        caption = "Quelle: BVS, Statistik Stadt Zürich") +
   ssz_theme(grid_lines = "y",
-            base_family = "HelveticaNeueLT Pro 55 Roman",
-            base_size = 12) +
+            base_size = 12,
+            base_family = "Helv") +
   theme(axis.title.y = element_text(
     margin = margin(t = 0, r = -43, b = 0, l = 0)
   ))
